f018a460de101319327a20b78ae48d0d01e42c4c
pem2k <mckilpar000@hotmail.com>
2025-11-20 11:31:21 -0800

    fixed eslint filepath error, setup inital routing for modular inverse calculator page, header now renders on main page

---
+++ src/components/InverseFormResult.jsx
@@ -0,0 +1,12 @@
+/*import React, { useState } from "react";
+
+export default function Inverse_form_result(){
+    const [number_input, set_number_input] = useState("");
+    const [mod_input, set_mod_input] = useState("");
+    const [steps, set_steps] = useState([]);
+    const [result, set_result] = useState("")
+}
+
+const submit_handler = (event) => {
+    event.preventDefault();
+}*/
---
+++ src/pages/Inverse.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import InverseFormResults from "../components/InverseFormResult"
+
+export default function Inverse(){
+    return(
+        <div className = "container mt-4">
+            <h2>Modular Inverse Calculator</h2>
+        </div>
+    )
+}
