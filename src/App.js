2b10b5eadd003b4afd07b5a3c9f1c1f489ccce9d
pem2k <mckilpar000@hotmail.com>
2025-11-20 12:14:51 -0800

    added placeholder navbar jsx

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
+import Navbar from './components/Navbar.jsx';
 import Home from "./pages/Home.jsx";
 import Inverse from "./pages/Inverse.jsx";
 import Cipher from "./pages/Cipher.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path ="/" element={<Home />}/>
-        <Route path ="/Inverse" element ={<Inverse />}/>
-        <Route path = "/Cipher" element={<Cipher />}/>
-      </Routes>
-    </Router>
+    <div>
+      <Navbar/>
+      <main>
+        <Router>
+          <Routes>
+            <Route path ="/" element={<Home />}/>
+            <Route path ="/Inverse" element ={<Inverse />}/>
+            <Route path = "/Cipher" element={<Cipher />}/>
+          </Routes>
+        </Router>
+    </main>
+    </div>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Inverse</a></li>
+        <li><a href="#">Cipher</a></li>
+      </ul>
+    </nav>
+  );
+}
