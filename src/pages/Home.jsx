3bbd759c2696dc78e4b5eb445df26f3a98b3a5f3
pem2k <mckilpar000@hotmail.com>
2025-11-28 13:28:59 -0800

    updated home page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,29 @@
-import React from "react"
+import React from "react";
 
-// considering writeup of entire project on web side, render latex with: https://www.npmjs.com/package/react-latex-next
+export default function Home() {
+  return (
+    <div className="container mt-1 fs-5">
+      <div className="card border-0 p-4">
 
-export default function Home(){
-    return(
-        <div className="mt-2">
-            <div className=" container">
-                <p>
-                    We implemented a function using the Extended Euclid's Algorithm to find the inverse of a modulo function using extended Euclidean Algorithm. The function can be accessed via this web app. It app produces all the works for a user provided problem.
-                </p>
-                <p>
-                    The definition of multiplicative inverse of A mod B, for two positive integers A and B, is not very far from the definition of multiplicative inverse of an integer. It is an integer x such that when we multiply a and A mod B we get 1.
-                </p>
-                <p>
-                    Ax mod B = 1
-                </p>
-            </div>
-        </div>
-    )
+        <p className="fs-5">
+          We implemented a function using the Extended Euclid's Algorithm to find the inverse of a modulo function using extended Euclidean Algorithm. The function can be accessed via this web app. It app produces all the works for a user provided problem.
+        </p>
+
+        <p className="fs-5">
+          The definition of multiplicative inverse of A mod B, for two positive integers A and B, is not very far from the definition of multiplicative inverse of an integer. It is an integer x such that when we multiply a and A mod B we get 1.
+        </p>
+
+        <p className="text-center fs-5">
+          Ax mod B = 1
+        </p>
+        <p className="text-left fs-6">
+        For a detailed description of the topic, please see Seymour Lipschutz and Marc L Lipson (2022):
+        </p>
+        <p className="text-left fs-6">
+         “Properties of the Integers”. In: Schaum’s Outline of Discrete Mathematics. 4th ed. Accessed via Northeastern University Library. New York: McGraw Hill, 2022. Chap. 11. url: <a href="https://www.accessengineeringlibrary.com.ezproxy.neu.edu/content/book/9781264258802/chapter/chapter11"></a>https://www.accessengineeringlibrary.com.ezproxy.neu.edu/content/book/9781264258802/chapter/chapter11
+        </p>
+      </div>
+      
+    </div>
+  );
 }
