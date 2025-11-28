5e121ab5c43568c4a0fa4b8bdf8ad83131a60576
pem2k <mckilpar000@hotmail.com>
2025-11-28 13:34:08 -0800

    variable name fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,16 +10,16 @@ export default function Home() {
         </p>
 
         <p className="fs-5">
-          The definition of multiplicative inverse of A mod B, for two positive integers A and B, is not very far from the definition of multiplicative inverse of an integer. It is an integer x such that when we multiply a and A mod B we get 1.
+          The definition of multiplicative inverse of A mod B, for two positive integers A and B, is not very far from the definition of multiplicative inverse of an integer. It is an integer x such that when we multiply x and A mod B we get 1.
         </p>
 
         <p className="text-center fs-5">
           Ax mod B = 1
         </p>
-        <p className="text-left fs-6">
+        <p className="text-left fs-5">
         For a detailed description of the topic, please see Seymour Lipschutz and Marc L Lipson (2022):
         </p>
-        <p className="text-left fs-6">
+        <p className="text-left fs-5">
          “Properties of the Integers”. In: Schaum’s Outline of Discrete Mathematics. 4th ed. Accessed via Northeastern University Library. New York: McGraw Hill, 2022. Chap. 11. url: <a href="https://www.accessengineeringlibrary.com.ezproxy.neu.edu/content/book/9781264258802/chapter/chapter11">https://www.accessengineeringlibrary.com.ezproxy.neu.edu/content/book/9781264258802/chapter/chapter11</a>
         </p>
       </div>
